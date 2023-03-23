2f168e8a6209a994ed7854b7839b67be94f5f2d6
Giorgi <88037076+GiorgiBG@users.noreply.github.com>
2023-03-23 10:16:40 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-# with open("website.html") as URL:
-#     contents = URL.read()
-#
-# soup = BeautifulSoup(contents, "html.parser")
-#
-# #print(soup.title.string)
-# paragraph = soup.find_all("ul")
-# for text in paragraph:
-#     print(text.getText())
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
+# Write your code below this line 👇
 
-found = requests.get("https://news.ycombinator.com/")
-
-URL = found.text
-soup = BeautifulSoup( URL, "html.parser")
-title_lists = soup.find("td", class_="title")
-# for answer in title_lists:
-#     link = answer.a
-#     print(link)
-
-found_article = soup.find_all("span", class_="titleline")
-article_text = [article.get_text() for article in found_article]
-print(article_text)
-
-article_link = [found_link.a['href'] for found_link in found_article]
-print(article_link)
-
-article_upvote = [int(score.get_text().split()[0]) for score in soup.find_all("span", class_="score")]
-max_vot0e_article_index = article_upvote.index(max(article_upvote))
-print(f" article name: {article_text[max_vot0e_article_index]} \n article link: {article_link[max_vot0e_article_index]} \n article code: {article_upvote[max_vot0e_article_index]}")
+response = requests.get(URL)
+found = response.text
+soup = BeautifulSoup(found, "html.parser")
+movie_title = soup.find_all("h3", class_="title")
+with open("movie_title.txt", "a") as file:
+    for title in movie_title:
+        file.write(f"{title.get_text()}\n")
